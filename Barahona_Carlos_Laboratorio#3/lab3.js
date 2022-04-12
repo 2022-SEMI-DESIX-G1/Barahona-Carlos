6e0338dbac468b4e3b7488036d024ba4e40556bc
Krlosz <103006650+Krlosz@users.noreply.github.com>
2022-04-11 19:22:54 -0500

    Update lab3.js

--- Barahona_Carlos_Laboratorio#3/lab3.js
+++ Barahona_Carlos_Laboratorio#3/lab3.js
@@ -2,20 +2,21 @@
 const Palindromo=document.querySelector('.palindromo')
 const decimal=document.querySelector('.decimal')
 const binario=document.querySelector('.binario')
-let str = "33"
+function getPalindromo(){
+let str = document.getElementById("palindromo").value
 function transform(str){
-    var num = parseInt(str)
+    let num = parseInt(str)
     return num.toString(2)
 }
    let num = transform(str)
 
    function Palindrome(str){
-     var strReversed = str.split("").reverse().join("");
+     let strReversed = str.split("").reverse().join("");
      return strReversed === str ? "Es palindromo" : "No es palindromo";
 }
-     var respuesta1 = Palindrome(str)
-      var respuesta2 = Palindrome(num)
-     var resupuesta4 = "no es palindromo de doble base"
+     let respuesta1 = Palindrome(str)
+      let respuesta2 = Palindrome(num)
+     //var resupuesta4 = "no es palindromo de doble base"
      if(respuesta1 == "Es palindromo" && respuesta2 =="Es palindromo") {
       var respuesta4 = "Es palindromo de doble base"
 }
@@ -26,24 +27,24 @@ console.log(str)
 console.log(respuesta1)
 decimal.innerText = "Base 10 " +respuesta1
 binario.innerText = "Base 2 " +respuesta2
-Palindromo.innerText = "La variable " + respuesta4
+Palindromo.innerText = "El numero "+str+" "+ respuesta4
 console.log(num)
 console.log(respuesta2)
 console.log(respuesta4)
-
+}
 //Problema #2--------------------------------------------------------------------------------
 const ctos = document.querySelector('.howmany')
 // Me estaba rompiendo la cabeza y solo tenia que mover un corchete .-.
 function getInput(){
- var texto = document.getElementById("a").value
+ let texto = document.getElementById("a").value
 //let texto = "AABBBACCAA";
 //console.log(texto)
 function cuentaLetras(txt){
     txt=txt.toLowerCase();
-    var arr={};
-    var n = texto.length;
+    let arr={};
+    let n = texto.length;
     for(i=0; i < n; i++){
-        var char=txt.charAt(i);
+        let char=txt.charAt(i);
         if (char==" ") continue;
         if (arr[char]) arr[char]++
         else arr[char]=1;
@@ -51,15 +52,15 @@ function cuentaLetras(txt){
     return arr;
 }
 function objetoACadena(arr){
-    var cadena=[];
-    for (var k in arr)
+    let cadena=[];
+    for (let k in arr)
     {
         cadena.push(k+"="+arr[k]);
     }
     return cadena.join(", ");
 }
-var contador=cuentaLetras(texto);
-var answ=objetoACadena(contador)
+let contador=cuentaLetras(texto);
+let answ=objetoACadena(contador)
 console.log(answ)
 console.log(objetoACadena(contador));
 ctos.innerText = `La cantidad de cada caracter es: ${answ}`;
@@ -68,32 +69,32 @@ ctos.innerText = `La cantidad de cada caracter es: ${answ}`;
 //Problema #3----------------------------------------------------------------------------
 const bis=document.querySelector('.bisiesto')
 function getInput2(){
-var año = document.getElementById("year").value
+let año = document.getElementById("year").value
 console.log(año)
 if (año%4==0&&año%100!=0)
 { 
-     var info1 = año + " El año es un año bisiesto";
+     let info1 = año + " El año es un año bisiesto";
     bis.innerText = info1  
 }
 else if (año%400==0)
 {  
-     var info2 = año + " El año es un año bisiesto";
+     let info2 = año + " El año es un año bisiesto";
      bis.innerText = info2  
 }  
 else
 {  
-    var info3 = año + " El año no es bisiesto";
+    let info3 = año + " El año no es bisiesto";
      bis.innerText = info3   
 } 
 }
 //Problema #4
 const sump=document.querySelector('.numprimo')
 function getInput3(){
- var int = document.getElementById("prime").value
+ let int = document.getElementById("prime").value
 console.log(int)
 function sumPrimes(n) {
-    var sum = 0;
-    for (var i = 0; i <= n; i++) {
+    let sum = 0;
+    for (let i = 0; i <= n; i++) {
         if (isPrime(i)) {
             sum += i;
         }
@@ -104,8 +105,8 @@ function sumPrimes(n) {
  function isPrime(n) {
     if (n < 2) { return false; }
     if (n !== Math.round(n)) { return false; }
-    var result = true;
-    for (var i = 2; i <= Math.sqrt(n); i++) {
+    let result = true;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
         if (n % i === 0) {
             result = false;
         }
@@ -113,7 +114,7 @@ function sumPrimes(n) {
     return result;
 }
 console.log(sumPrimes(int))
-sump.innerText ="La suma de los numeros Primos del 0 al "+int +"es igual a: " + sumPrimes(int)
+sump.innerText ="La suma de los numeros Primos del 0 al"+" "+int +"es igual a: " + sumPrimes(int)
 }
 
 
